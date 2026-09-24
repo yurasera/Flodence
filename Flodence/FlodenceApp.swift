c0975d8a9c271f24e2cd103cabc71429a3d24166
yurasera <yuhaya.lissera@gmail.com>
2026-09-24 20:11:35 +0700

    Initial Commit

---
+++ Flodence/FlodenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlodenceApp.swift
+//  Flodence
+//
+//  Created by Yuhaya Lissera on 24/09/26.
+//
+
+import SwiftUI
+
+@main
+struct FlodenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
